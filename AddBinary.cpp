fb2efbccc33da8f38713845008e8dbb88ba7cfce
kajalpunia <70537516+kajalpunia@users.noreply.github.com>
2021-06-29 23:26:27 +0530

    Add files via upload

---
+++ AddBinary.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string addBinary(string a, string b) 
+    {
+        int aBi = a.length() - 1, bBi = b.length() - 1, carry = 0;
+        string res = "";
+        
+        while(aBi >= 0 || bBi >= 0 || carry)
+        {
+            int sum = ((aBi >= 0) ? a[aBi] - '0' : 0) + \
+                      ((bBi >= 0) ? b[bBi] - '0' : 0) + \
+                      carry;            
+            res.insert(res.begin(), (sum%2 + '0'));            
+            carry = sum/2;             
+            --aBi;
+            --bBi;
+        }        
+        return res;
+    }
+};
