15a556ce51b6422c0c999966f2ed9fc39ac58b64
kajalpunia <70537516+kajalpunia@users.noreply.github.com>
2021-07-17 23:11:07 +0530

    Add files via upload

---
+++ MaximumSubarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) 
+    {
+         int max_so_far = nums[0];
+        int result = nums[0];
+        for(int i=1; i<nums.size(); i++)
+        {
+            max_so_far = max(nums[i], nums[i] + max_so_far);
+            result = max(result, max_so_far);
+        }
+        return result+1;
+    }
+};
