9398c3ca9444e71185f047ec21ea9bfedcfc8873
kajalpunia <70537516+kajalpunia@users.noreply.github.com>
2021-08-03 22:16:58 +0530

    Add files via upload

---
+++ PowerOfFour.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) 
+    {
+        if(n==1)
+           return true;
+        if(n==4)
+            return true;
+        if(n<4)
+            return false;
+        return (n%4==0)?isPowerOfFour(n/4):false;
+    }
+};
