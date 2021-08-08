69734bfe088cfff569c88070190d9853cb1b2f09
kajalpunia <70537516+kajalpunia@users.noreply.github.com>
2021-08-08 19:08:15 +0530

    Add files via upload

---
+++ CountingBits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> countBits(int n) 
+    {
+       vector<int>dp(n+1);
+    if(n==0)
+        return dp;        
+    dp[0]=0;
+    dp[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        if(i%2==0)
+        {
+            dp[i]=dp[i/2];
+        }
+        else
+        {
+            dp[i]=dp[i-1]+1;
+        }
+    }
+    return dp; 
+    }
+};
